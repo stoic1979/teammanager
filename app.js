b5ebee8eeb7081c2827ba795ed69e1916feca1e3
Navjot Singh <weavebytes@gmail.com>
2017-10-15 17:37:22 +0530

    basic jade views

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ var mongoose = require('mongoose');
 //  SETUP APP
 //-----------------------------------------------------
 var app = express();
-app.use(express.static('public'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+//app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
