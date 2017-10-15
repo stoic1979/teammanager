b830bdd2791d159cf8d7f5c3ba711180dd11908d
Navjot Singh <weavebytes@gmail.com>
2017-10-15 15:42:39 +0530

    use params from env

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const TEAM_MANAGER_MONGODB_URI = process.env.TEAM_MANAGER_MONGODB_URI;
+const TEAM_MANAGER_PORT = process.env.TEAM_MANAGER_PORT;
 
 //-----------------------------------------------------
 //   CONNECT TO MONGODB
 //-----------------------------------------------------
-mongoose.connect(config.database, function(err) {
+mongoose.connect(TEAM_MANAGER_MONGODB_URI, function(err) {
 	if(err) {
 		console.log("[ERROR] failed to connect to database: " + err);
 	} else {
@@ -22,3 +24,27 @@ mongoose.connect(config.database, function(err) {
 	}
 });
 
+//-----------------------------------------------------
+//   APP ROUTES
+//-----------------------------------------------------
+
+app.get('/', function (req, res) {
+   	res.sendFile( __dirname + "/public/views/" + "index.html" );
+})
+
+
+//-----------------------------------------------------
+//                    START SERVER 
+//-----------------------------------------------------
+var server = app.listen(TEAM_MANAGER_PORT, function (err) {
+   
+	if(err) {
+		console.log(err);
+	} else {
+
+   		var host = server.address().address
+   		var port = server.address().port
+   
+   		console.log("Example app listening at http://%s:%s", host, port)
+	}
+})
