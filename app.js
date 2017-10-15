39881b97eb768f13ab8f191da2998b79e7913fe8
Navjot Singh <weavebytes@gmail.com>
2017-10-15 17:57:06 +0530

    updated jade tmpls; added static files

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-//app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
