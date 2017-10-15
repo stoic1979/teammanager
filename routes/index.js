d0864c395ba807a87e2dffea00f139f48656754e
Navjot Singh <weavebytes@gmail.com>
2017-10-15 18:48:37 +0530

    jade tmpls for login/register

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Team Manager' });
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Team Manager' });
+});
+
 module.exports = router;
