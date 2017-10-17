5436f8555f11a0a83a15b11db8da9332e1e4a1f8
Navjot Singh <weavebytes@gmail.com>
2017-10-17 22:36:40 +0530

    task schema and dummy ui

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,22 @@ function getUserData(req) {
 
   data.title = 'Team Manager';
 
+
+  // dummy tasks
+  var tasks = [
+     {
+       title: 'fix bug 1',
+       worker: 'tom'
+     },
+          {
+       title: 'add feature 1',
+       worker: 'marry'
+     },
+
+  ];
+
+  data.tasks = tasks;
+
   return new Promise(function(resolve, reject) {  //Promise 1: get projects 
     if(!req.session.user) {
       resolve(data);
---
+++ schema/task.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const mongoose = require("mongoose");
+
+//--------------------------------------------------------------------
+// Task schema definition
+//--------------------------------------------------------------------
+const TaskSchema = new mongoose.Schema({
+  worker: {
+  	type: mongoose.Schema.Types.ObjectId, 
+  	ref: 'User'
+  },	
+  title: { 
+    type: String,
+    required: true
+  },
+  description: { 
+    type: String,
+    required: true
+  },
+  created_at: Date,
+  updated_at: Date
+});
+
+//--------------------------------------------------------------------
+// We can use the Schema pre method to have operations happen before
+// an object is saved
+//--------------------------------------------------------------------
+TaskSchema.pre('save', function(next){
+
+    var task = this; // this refers to TaskSchema object
+
+    var currentDate = new Date();
+
+    // change the updated_at field to current date
+    task.updated_at = currentDate;
+
+    // if created_at doesn't exist, add to that field
+    // otherwise, only update_at will be set to current date
+    if (!task.created_at) {
+       task.created_at = currentDate;
+    }
+
+    next();
+
+});
+
+module.exports = mongoose.model("Task", TaskSchema);
