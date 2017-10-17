89fd98be3f6ee863a2803928c91d0359ff5ab975
Navjot Singh <weavebytes@gmail.com>
2017-10-17 15:12:45 +0530

    refactored code; ui refined

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({defaultLayout: 'main', extname: '.html'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
