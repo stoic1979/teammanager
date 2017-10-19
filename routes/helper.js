3027221879afb2e6956e57600c829c653a18caa6
Navjot Singh <weavebytes@gmail.com>
2017-10-19 10:11:41 -0400

    get tasks for project and send to / route

--- routes/helper.js
+++ routes/helper.js
@@ -1,6 +1,42 @@
 var Project = require('../schema/project');
+var Task = require('../schema/task');
 
 
+
+function getUserProjects(req, data) {
+
+
+  console.log("[INFO] ::  getUserProjects()");
+
+
+  return new Promise(function(resolve, reject) {  //Promise 1: get projects 
+
+    Project.find({manager: req.session.user._id}).exec().
+    then(function(projects) {
+      
+      resolve(data);
+    });
+
+  }); //Promise 1: get projects 
+}
+
+function getProjectTasks(project_id) {
+
+  console.log("[INFO] ::  getProjectTasks()");
+
+  return new Promise(function(resolve, reject) {  //Promise 1: get tasks of current project 
+
+    Task.find({project: project_id}).exec().
+    then(function(tasks) {
+      resolve(tasks);
+    }).
+    catch(function(err) {
+         // no tasks added to this project or project no found
+         console.log("[ERROR] :: getProjectTasks, err: " + err);
+         resolve([]);
+    });
+  }); //Promise 1: get projects 
+}
 
 
 //-----------------------------------
@@ -20,7 +56,7 @@ function getUserData(req) {
        title: 'fix bug 1',
        worker: 'tom'
      },
-          {
+    {
        title: 'add feature 1',
        worker: 'marry'
      },
@@ -29,19 +65,25 @@ function getUserData(req) {
 
   data.tasks = tasks;
 
-  return new Promise(function(resolve, reject) {  //Promise 1: get projects 
-    if(!req.session.user) {
-      resolve(data);
-    } 
-
-    data.user = req.session.user;
-    Project.find({manager: req.session.user._id}).exec().
-    then(function(projects) {
-      data.projects = projects;
+  if(!req.session.user) {
+    return new Promise(function(resolve, reject) {
       resolve(data);
     });
+  }
 
-  }); //Promise 1: get projects 
+  data.user = req.session.user;
+
+  project_id = "59e622e36af4fb62d7286eab";
+
+  return getUserProjects(req).
+  then(function(projects){
+      data.projects = projects;
+      return getProjectTasks(project_id);
+  })
+  .then(function(tasks){
+      data.tasks = tasks;
+      return data;
+  })
 }
 
 
