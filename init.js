b489cba03a21d3e57b7d2ac5e8a79e8efdfa8fc0
Navjot Singh <weavebytes@gmail.com>
2017-10-31 10:52:36 +0530

    WORKFLOW FOR SEQUENCE OF INITIALIZATION OF PROJECT

---
+++ init.js
@@ -0,0 +1,29 @@
+//-------------------------------------------------------------
+// SAMPLE WORKFLOW FOR SEQUENCE OF INITIALIZATION OF PROJECT
+//-------------------------------------------------------------
+
+//FIXME - add actual workflow later
+
+var workflow = new (require('events').EventEmitter)();
+var async = require('async');
+
+workflow.on('doTask1', function() {
+    console.log("[INFO] doing task 1....");
+    return workflow.emit('doTask2');
+});
+
+workflow.on('doTask2', function() {
+    console.log("[INFO] doing task 2....");
+    return workflow.emit('doTask3');
+});
+
+workflow.on('doTask3', function() {
+    console.log("[INFO] doing task 3....");
+    return workflow.emit('workflowCompleted');
+});
+
+workflow.on('workflowCompleted', function() {
+    console.log("[INFO] workflow completed....");
+});
+
+workflow.emit('doTask1');
