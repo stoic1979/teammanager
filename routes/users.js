b500f1615f4a5bd963d8a0529d64d081fabdfa3f
Navjot Singh <weavebytes@gmail.com>
2017-12-27 12:03:10 +0530

    updated json resp for login

--- routes/users.js
+++ routes/users.js
@@ -134,6 +134,11 @@ router.post('/login', function(req, res) {
                 res.json({
                     success: true,
                     message: "Successfully login !",
+                    role: user.role,
+                    user_id: user._id,
+                    email: user.email,
+                    first_name: user.first_name,
+                    last_name: user.last_name,
                     token: token
                 });
 
